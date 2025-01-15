9a88477c4a131d201d0656d8c2a4d9d1c4766eac
charlie4419 <charlie@helloipju.com>
2025-01-15 12:59:11 +0900

    일단 테스트 제외

--- src/chat/chat.gateway.spec.ts
+++ src/chat/chat.gateway.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 describe('ChatGateway', () => {
   let gateway: ChatGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [AuthModule],
       providers: [ChatGateway, ChatService],
     }).compile();
 
--- src/chat/chat.service.spec.ts
+++ src/chat/chat.service.spec.ts
@@ -1,15 +1,23 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { ChatService } from './chat.service';
+import { Chat } from './entity/chat.entity';
+import { ChatRoom } from './entity/chat-room.entity';
+import { Repository } from 'typeorm';
+import { TestBed } from '@automock/jest';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
 
 describe('ChatService', () => {
   let service: ChatService;
+  let chatRepository: jest.Mocked<Repository<Chat>>;
+  let chatRoomRepository: jest.Mocked<Repository<ChatRoom>>;
+  let userRepository: jest.Mocked<Repository<User>>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ChatService],
-    }).compile();
-
-    service = module.get<ChatService>(ChatService);
+    const { unit, unitRef } = TestBed.create(ChatService).compile();
+    service = unit;
+    chatRepository = unitRef.get(getRepositoryToken(Chat) as string);
+    chatRoomRepository = unitRef.get(getRepositoryToken(ChatRoom) as string);
+    userRepository = unitRef.get(getRepositoryToken(User) as string);
   });
 
   it('should be defined', () => {
